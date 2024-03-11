658624dd3d47df122943d998cb72955544eae697
Betul <betulkoru06@gmail.com>
2024-03-11 15:06:10 +0300

    tailwind text classes

--- tailwind-css/src/App.js
+++ tailwind-css/src/App.js
@@ -1,9 +1,11 @@
+import Typography from "./components/Typography";
 
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-2xl text-center text-purple-700 font-bold">Hello Tailwind</h1>
+      <Typography />
     </div>
   );
 }
---
+++ tailwind-css/src/components/Typography.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Typography = () => {
+  return (
+    <div>
+        <h3 className='text-2xl '>Typography</h3>
+        <p className="text-sm">Typography</p>
+        <p className='test-base/7'>Typography</p>
+        <p className='text-lg'>Typography</p>
+        <p className='text-xl'>Typography</p>
+        <p className='text-2xl'>Typography</p>
+        <h2 className='text-3xl'>Typography</h2>
+        <h1 className='text-[60px]'>Title</h1>
+        <article>
+            <p className='line-clamp-2'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, reprehenderit ipsa. Nostrum omnis quibusdam exercitationem fugit molestias asperiores officia voluptates vero animi, perspiciatis recusandae enim commodi deserunt necessitatibus esse consectetur? Iusto natus quasi ab recusandae dolore expedita quod aliquam ipsam soluta excepturi voluptates, eum, eaque beatae ea quisquam eveniet quia dolorum veniam delectus animi. Ullam iure corrupti ex dolores alias, quas perferendis numquam sed eum vero obcaecati omnis voluptatum. Impedit mollitia consequuntur porro illum maxime provident nesciunt dignissimos eligendi ipsa animi reiciendis iure voluptatum, ea sit ullam dolorem quas minus minima rem. Nisi, atque! Recusandae maxime sint sit nobis placeat?
+            </p>
+        </article>
+        <p className='text-center'>hello hi</p>
+        <p className='underline'>hello hi</p>
+        <p className='line-through'>hello hi</p>
+        <p className='uppercase'>hello hi</p>
+        <p className='lowercase'>hello hi</p>
+        <p className="text-red-400">Merhaba</p>
+        <p className="text-[#A1EEBD]">Merhaba</p>
+        <p className="text-[rgba(73,163,186,0.886)]">Merhaba</p>
+    </div>
+  )
+}
+
+export default Typography
