b19ffab7dc035fd68db5820be1512e5d88329203
Betul <betulkoru06@gmail.com>
2024-03-12 16:07:55 +0300

    tailwinf hover and focus

--- tailwind-css/src/App.js
+++ tailwind-css/src/App.js
@@ -1,13 +1,15 @@
-import SpacingAndSizing from "./components/SpacingAndSizing";
+import HoverFocus from "./components/HoverFocus";
+// import SpacingAndSizing from "./components/SpacingAndSizing";
 // import Typography from "./components/Typography";
 
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-2xl text-center text-purple-700 font-bold">Hello Tailwind</h1>
+      {/* <h1 className="text-2xl text-center text-purple-700 font-bold">Hello Tailwind</h1> */}
       {/* <Typography /> */}
-      <SpacingAndSizing />
+      {/* <SpacingAndSizing /> */}
+      <HoverFocus />
     </div>
   );
 }
