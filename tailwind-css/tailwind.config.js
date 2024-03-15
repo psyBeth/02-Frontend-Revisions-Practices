61a83d5f12ad91aa0d5d1703f3f42647951779aa
Betul <betulkoru06@gmail.com>
2024-03-16 00:58:05 +0300

    dark mode issue fixed

--- tailwind-css/tailwind.config.js
+++ tailwind-css/tailwind.config.js
@@ -7,9 +7,24 @@ module.exports = {
   //     "./node_modules/flowbite/**/*.js"
   // ],
   theme: {
-    extend: {},
+
+    //* If we do not want to add our own instead of tailwind's default theme settings, we must add it in the extend object, otherwise the entire theme object will change and we cannot use anything other than the ones we added.
+    //! dark mode etc. will not work if this is not added!!!!!!!
+
+    extend: {
+      colors: {
+        primary: "#AC87C5",
+        primary_hover: "#756AB6",
+        dark: "#526D82",
+        darker: "#27374D",
+        blue: {
+          950: "#17275c",
+        },
+      },
+    },
   },
   //* when we want to use a plugin, it must be added to config file
   // plugins: [ require('flowbite/plugin')],
+  darkMode: "class",
 }
 
