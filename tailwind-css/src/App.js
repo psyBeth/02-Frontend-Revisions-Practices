f3c0ce1cbf2833ad04f281693024458de3191827
Betul <betulkoru06@gmail.com>
2024-03-15 23:38:39 +0300

    navbar with a dark mode switch (error occured)

--- tailwind-css/src/App.js
+++ tailwind-css/src/App.js
@@ -1,16 +1,19 @@
 // import HoverFocus from "./components/HoverFocus";
 // import SpacingAndSizing from "./components/SpacingAndSizing";
 // import Typography from "./components/Typography";
-import FlexAndResponsive from "./components/FlexAndResponsive"
+// import FlexAndResponsive from "./components/FlexAndResponsive"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       {/* <h1 className="text-2xl text-center text-purple-700 font-bold">Hello Tailwind</h1> */}
       {/* <Typography /> */}
       {/* <SpacingAndSizing /> */}
       {/* <HoverFocus /> */}
-      <FlexAndResponsive />
+      {/* <FlexAndResponsive /> */}
+     
     </div>
   );
 }
