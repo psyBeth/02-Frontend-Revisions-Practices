c7fcf9e2ecab4662098a024933618a0a2116db41
Betul <betulkoru06@gmail.com>
2024-03-18 02:48:00 +0300

    edited to private route

---
+++ rc14-contextapi/src/context/LoginContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginContext = () => {
+  return (
+    <div>LoginContext</div>
+  )
+}
+
+export default LoginContext
--- rc14-contextapi/src/pages/PrivateRouter.jsx
+++ rc14-contextapi/src/pages/PrivateRouter.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRouter = () => {
+    //TODO: make it read from global state
     const user = true;
     return user ? <Outlet /> : <Navigate to="/login" />;
 };
