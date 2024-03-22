32b8725e77d170d2585bf258c913e318ab536764
Betul <betulkoru06@gmail.com>
2024-03-22 03:45:40 +0300

    trying things that obviously doesn't work out

--- rc14-reducer/src/useReducerExample.js
+++ rc14-reducer/src/useReducerExample.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useState, useReducer } from "react"
+import { reducer } from "./reducer"
+import { initialState } from "./reducer"
 
-const useReducerExample = () => {
+const UseStateExample = () => {
+  // const [catImage, setCatImage] = useState("")
+  // const [error, setError] = useState("")
+  // const [loading, setLoading] = useState(false)
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const { loading, error, catImage } = state
+
+  const getCatImage = async () => {
+    const url = "https://api.thecatapi.com/v1/images/search"
+    // setLoading(true)
+    //? 1. START
+    dispatch({ type: "START", payload: "" })
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      // setCatImage(data[0].url)
+      // setError("")
+      //? 2. IF SUCCEED
+      dispatch({ type: "SUCCESS", payload: data[0].url })
+    } catch (error) {
+      // setError("DATA CAN NOT BE FETCHED")
+      // setCatImage("")
+      //? 3. IF FAILED
+      dispatch({ type: "FAIL", payload: "DATA CAN NOT BE FETCHED" })
+      console.log(error)
+    }
+  }
+  console.log(error)
+  console.log(catImage)
+
   return (
-    <div>useReducerExample</div>
+    <div>
+      <button
+        onClick={getCatImage}
+        disabled={loading}
+        style={{ display: "block", margin: "1rem" }}
+      >
+        Get Cat Image
+      </button>
+      {error && <h2>{error}</h2>}
+      {catImage && <img width="50%" src={catImage} alt="img" />}
+    </div>
   )
 }
 
-export default useReducerExample
+export default UseStateExample
