32d2e9e6c914cab68bd9dc9045217c39fde5abf4
Betul <betulkoru06@gmail.com>
2024-05-23 13:57:48 +0300

    authSlice

---
+++ rc21-redux-2/src/features/authSlice.jsx
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: { email: "", password:""},
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload
+        },
+
+        clearUser: (state) => {
+            state.user = initialState
+        }
+    }
+});
+
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
