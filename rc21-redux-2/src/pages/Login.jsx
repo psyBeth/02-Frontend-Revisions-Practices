96e88fec0c39f9c33bd3d337902bea33b8c99a4a
Betul <betulkoru06@gmail.com>
2024-05-24 12:00:46 +0300

    routing to login !user

--- rc21-redux-2/src/pages/Login.jsx
+++ rc21-redux-2/src/pages/Login.jsx
@@ -7,16 +7,21 @@ import Avatar from "@mui/material/Avatar"
 import Typography from "@mui/material/Typography"
 import Container from "@mui/material/Container"
 import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { setUser } from "../features/authSlice"
+import { useNavigate } from "react-router-dom"
 
 export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // TODO: 
-        // storing login info in global store
-        // routing process
+        dispatch(setUser({ email, password }));
+
+        navigate("/");
         setEmail("");
         setPassword("");
     };
