67ea08d102c55bf8f51a0a6266c64c38cb7f45fb
Betul <betulkoru06@gmail.com>
2024-05-26 11:04:24 +0300

    explanation comments in authSlice

--- rc21-redux-2/src/features/authSlice.jsx
+++ rc21-redux-2/src/features/authSlice.jsx
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     user: { email: "", password:""},
 };
+//? With Slice, we can create both action types, action creator functions and reducer of a global state in a single move.
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
+        //? for the action creator func. and reducer
         setUser: (state, action) => {
             state.user = action.payload
         },
@@ -18,5 +20,8 @@ const authSlice = createSlice({
     }
 });
 
+//? to export the action functions -> destructure from sliceName.actions
 export const { setUser, clearUser } = authSlice.actions;
+
+//? to export the reducer of the slice -> sliceName.reducer
 export default authSlice.reducer;
