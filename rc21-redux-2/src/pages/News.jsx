1ad4b05e661505fb3c9a1f166a48c28813c6f3d9
Betul <betulkoru06@gmail.com>
2024-05-26 13:53:18 +0300

    don't touch my api

--- rc21-redux-2/src/pages/News.jsx
+++ rc21-redux-2/src/pages/News.jsx
@@ -5,11 +5,40 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { CardMedia } from "@mui/material";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearNewsData, getNewsData } from "../features/newApiSlice";
+import loadingGif from "../assets/loading.gif"
 
 const News = () => {
+    const dispacth = useDispatch();
+    const { newsData, error, loading } = useSelector((state) => state.newsApi);
+
+    //? start api request right after news component renders in DOM
+    useEffect(() => {
+        dispacth(getNewsData());
+
+        //? clear global state dispatch (after component unmounts from DOM)
+        return () => {
+            dispacth(clearNewsData());
+        };
+    }, []);
+
     return (
         <>
             <h1>NEWS</h1>
+            {loading && (
+                <Box display="flex" alignItems="center" justifyContent="center">
+                    <img src={loadingGif} />
+                </Box>
+            )}
+
+            {error && (
+                <Typography variant="h3" color="error" align="center">
+                    News can not be fetchd
+                </Typography>
+            )}
+
             <Box
                 xs={{ d: "flex" }}
                 display="flex"
@@ -17,7 +46,7 @@ const News = () => {
                 justifyContent="space-evenly"
                 flexWrap="wrap"
             >
-                {[1, 2, 3].map((item, index) => (
+                {newsData?.map((item, index) => (
                     <Card sx={{ maxWidth: 345, m: 5, maxHeight: 600 }} key={index}>
                         <CardMedia
                             component="img"
